82e04e8ab7249ea7558dab4a14fd62c89d43c99c
mase <mark.schenk@tomtom.com>
2014-07-01 17:28:04 +0200

    Added support for Design by Contract

---
+++ common/src/main/java/org/agileDev/common/annotations/OvalGuard.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2014 uniknow. All rights reserved.
+ * 
+ * This Java class is subject of the following restrictions:
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if any, must
+ * include the following acknowledgment: "This product includes software
+ * developed by uniknow." Alternately, this acknowledgment may appear in the
+ * software itself, if and wherever such third-party acknowledgments normally
+ * appear.
+ * 
+ * 4. The name ''uniknow'' must not be used to endorse or promote products
+ * derived from this software without prior written permission.
+ * 
+ * 5. Products derived from this software may not be called ''UniKnow'', nor may
+ * ''uniknow'' appear in their name, without prior written permission of
+ * uniknow.
+ * 
+ * THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL WWS OR ITS
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.agileDev.common.annotations;
+
+import net.sf.oval.exception.ExceptionTranslatorJDKExceptionsImpl;
+import net.sf.oval.guard.GuardAspect2;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class OvalGuard extends GuardAspect2 {
+
+    public OvalGuard() {
+        super();
+
+        // specify an exception translator. Translates OVal specific
+        // exceptions to standard exceptions part of the JRE:
+        // -. ConstraintsViolatedException for constructor/method parameter
+        // translated to IllegalArgumentException
+        // -. ConstraintsViolatedException for class field translated to
+        // IllegalStateException
+        // -. ConstraintsViolatedException for method return values translated
+        // to IllegalStateException
+        // -. Other exceptions based on OValException translated to
+        // RuntimeException
+        getGuard().setExceptionTranslator(
+            new ExceptionTranslatorJDKExceptionsImpl());
+    }
+
+}
