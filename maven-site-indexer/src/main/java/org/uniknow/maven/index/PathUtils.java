c14f28fb94bcc9e0fa4ebae0abab2c53bb00c6f5
mase <mark.schenk@tomtom.com>
2015-04-08 15:50:59 +0200

    Corrected groupdId maven site indexer

--- maven-site-indexer/src/main/java/org/uniknow/maven/index/PathUtils.java
+++ maven-site-indexer/src/main/java/org/uniknow/maven/index/PathUtils.java
@@ -37,18 +37,53 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package org.apache.maven.siteindexer.test;
+package org.uniknow.maven.index;
 
-public class IndexerTest {
-    // @Test
-    // public void indexTest() {
-    // Indexer indexer = new Indexer();
-    // try {
-    // indexer.buildIndex(
-    // "C:\\Development\\Apache\\directmemory\\trunk\\target\\site\\",
-    // "index.js");
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // }
-    // }
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+
+/**
+ * Contains utility methods for getting {@code Path} instances
+ */
+public class PathUtils {
+
+    /**
+     * Finds a resource with a given name.
+     * 
+     * @param name
+     *            name of the desired resource
+     * @return A {@link java.nio.file.Path} object or {@code null} if no
+     *         resource with this name is found
+     */
+    public static final Path getResourceAsPath(String name) {
+        Path resourcePath = null;
+        try {
+            // Get path search box from classpath
+            URI uriToSearchBox = PathUtils.class.getResource(name).toURI();
+
+            final String[] parts = uriToSearchBox.toString().split("!");
+
+            FileSystem fs = null;
+            try {
+                fs = FileSystems.getFileSystem(URI.create(parts[0]));
+            } catch (FileSystemNotFoundException err) {
+                fs = FileSystems.newFileSystem(URI.create(parts[0]),
+                    new HashMap<String, String>());
+            }
+
+            resourcePath = fs.getPath(parts[1]);
+
+        } catch (URISyntaxException err) {
+            return null;
+        } catch (IOException err) {
+            return null;
+        }
+        return resourcePath;
+    }
 }
