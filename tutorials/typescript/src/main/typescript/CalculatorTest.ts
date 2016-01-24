71e02864e3f1cdd51e3edd057e358eaba65aca2b
mase <info.uniknow@gmail.com>
2016-01-24 09:20:10 +0100

    Replaced definition of var's Suite, Fact, Theory by import

--- tutorials/typescript/src/main/typescript/CalculatorTest.ts
+++ tutorials/typescript/src/main/typescript/CalculatorTest.ts
@@ -1,13 +1,9 @@
 /// <reference path="/Users/mase/Development/WorkSpace/uniknow/AgileDev/tutorials/typescript/node_modules/type-unit/dist/type-unit.d.ts" />
 /// <reference path="/Users/mase/Development/WorkSpace/uniknow/AgileDev/tutorials/typescript/node_modules/type-unit/dist/index"/>
-import typeunit = require("type-unit");
+import {Suite, Fact, Theory} from 'type-unit';
 import assert = require("assert");
 
 import {Calculator} from "./Calculator";
-
-var Fact = typeunit.Fact,
-    Theory = typeunit.Theory,
-    Suite = typeunit.Suite;
 
 @Suite("Calculator")
 class CalculatorTests {
