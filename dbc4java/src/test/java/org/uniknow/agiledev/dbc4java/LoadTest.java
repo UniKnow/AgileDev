f3baafafa9edccce28fad6ad40dd46624ecd4c84
mase <info.uniknow@gmail.com>
2017-03-31 08:11:35 +0200

    Added basic load testing for dbc4java. Results of load test are persisted within /target/contiperf-report. For now the acceptable average is set to 10ms. Require further investigation whether this is a proper value.

---
+++ dbc4java/src/test/java/org/uniknow/agiledev/dbc4java/LoadTest.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright (C) 2014 uniknow. All rights reserved.
+ * 
+ * This Java class is subject of the following restrictions:
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if any, must
+ * include the following acknowledgment: "This product includes software
+ * developed by uniknow." Alternately, this acknowledgment may appear in the
+ * software itself, if and wherever such third-party acknowledgments normally
+ * appear.
+ * 
+ * 4. The name ''uniknow'' must not be used to endorse or promote products
+ * derived from this software without prior written permission.
+ * 
+ * 5. Products derived from this software may not be called ''UniKnow'', nor may
+ * ''uniknow'' appear in their name, without prior written permission of
+ * uniknow.
+ * 
+ * THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL WWS OR ITS
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.uniknow.agiledev.dbc4java;
+
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.databene.contiperf.junit.ParallelRunner;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.uniknow.agiledev.dbc4java.examples.ExampleValidationConstraint;
+
+import javax.validation.ConstraintViolationException;
+import java.util.Random;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Performs basic load test of dbc4java
+ */
+public class LoadTest {
+
+    @Rule
+    public ContiPerfRule rule = new ContiPerfRule();
+
+    private InvariantsExample instanceWithInvariants = new InvariantsExample(5);
+    private ExampleValidationConstraint instanceWithConstrains = new ExampleValidationConstraint();
+
+    /**
+     * Creates class with acceptable value for invariant constraint.
+     */
+    @Test
+    @PerfTest(invocations = 1000000, rampUp = 100, threads = 100)
+    @Required(average = 10)
+    public void testConstructorWithAcceptableValueInvariantConstraints() {
+        new InvariantsExample(5);
+    }
+
+    /**
+     * Creates class with unacceptable value for invariant constraint.
+     */
+    @Test
+    @PerfTest(invocations = 1000000, rampUp = 100, threads = 100)
+    @Required(average = 10)
+    public void testConstructorWithUnacceptableInvariantConstraints() {
+        try {
+            new InvariantsExample(15);
+        } catch (ConstraintViolationException error) {
+            // Expected failure
+        }
+    }
+
+    /**
+     * Creates class with unacceptable value for invariant constraint in class
+     * that it extends.
+     */
+    @Test
+    @PerfTest(invocations = 1000000, rampUp = 100, threads = 100)
+    @Required(average = 10)
+    public void testConstructorExtendedClassWithUnacceptableInvariantConstraints() {
+        try {
+            new ExtendInvariantExample("TEST", null);
+        } catch (ConstraintViolationException error) {
+            // Expected failure
+        }
+    }
+
+    /**
+     * Creates class with acceptable value for invariant constraint in class
+     * that it extends.
+     */
+    @Test
+    @PerfTest(invocations = 1000000, rampUp = 100, threads = 100)
+    @Required(average = 10)
+    public void testConstructorExtendedClassWithAcceptableInvariantConstraints() {
+        new ExtendInvariantExample("TEST", "OTHER-TEST");
+    }
+
+    /**
+     * Set unacceptable value for instance invariant constraint.
+     */
+    @Test
+    @PerfTest(invocations = 1000000, rampUp = 100, threads = 100)
+    @Required(average = 10)
+    public void testSetUnacceptableValueInvariantConstraints() {
+        try {
+            instanceWithInvariants.setValue(15);
+        } catch (ConstraintViolationException error) {
+            // Expected failure
+        }
+    }
+
+    /**
+     * Set acceptable value for instance invariant constraint.
+     */
+    @Test
+    @PerfTest(invocations = 1000000, rampUp = 100, threads = 100)
+    @Required(average = 10)
+    public void testSetAcceptableValueInvariantConstraints() {
+        instanceWithInvariants.setValue(7);
+    }
+
+    /**
+     * Set value of instance invoking method with pre conditions
+     */
+    @Test
+    @PerfTest(invocations = 1000000, rampUp = 100, threads = 100)
+    @Required(average = 10)
+    public void testSetAcceptableValuePostCondition() {
+        instanceWithConstrains.setQuantity(7);
+    }
+
+    /**
+     * Set invalid value of instance invoking method with pre conditions
+     */
+    @Test
+    @PerfTest(invocations = 1000000, rampUp = 100, threads = 100)
+    @Required(average = 10)
+    public void testSetUnacceptableValuePostCondition() {
+        try {
+            instanceWithConstrains.setQuantity(15);
+        } catch (ConstraintViolationException error) {
+            // Expected failure
+        }
+    }
+
+}
